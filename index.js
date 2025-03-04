c8d4695ccee12b0bdaaaedeb183e09d5cfe850a9
tayuya74 <lyna-93@mail.ru>
2025-03-04 16:50:12 +0500

    add dark+light mode

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const button = document.querySelector(".start-button");
 	const stopButton = document.querySelector(".stop-button");
   const timeRange = document.getElementById("time-range");
+	const themeToggleButton = document.querySelector(".theme-toggle-button");
 	let canStart = true;
 	let countdown
 
   button.addEventListener("click", function () {
+		//TODO когда изменю селект для стилизации
 		const selectedTime = parseInt(timeRange.value);
     const cycles = Math.floor(selectedTime * 60 / 19); // 19 секунд на один цикл (4+7+8)
     if (canStart) {
@@ -14,6 +16,27 @@ document.addEventListener("DOMContentLoaded", function () {
 		timeRange.classList.remove("active");
 		stopButton.classList.add("active");
 		canStart = false;
+  });
+/* dark-mode */
+// Проверяем, есть ли сохраненная тема
+	if (localStorage.getItem("theme") === "dark") {
+		document.body.classList.add("dark-mode");
+		themeToggleButton.textContent = "dark_mode";
+	} else {
+		themeToggleButton.textContent = "light_mode";
+	}
+
+// Обработчик клика по кнопке
+	themeToggleButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+		// Проверяем, какая тема активна, и сохраняем её в localStorage
+		if (document.body.classList.contains("dark-mode")) {
+			localStorage.setItem("theme", "dark");
+			themeToggleButton.textContent = "dark_mode";
+		} else {
+			localStorage.setItem("theme", "light");
+			themeToggleButton.textContent = "light_mode";
+		}
   });
 
   function startBreathingCycle(cycles) {
@@ -37,11 +60,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function runTimer(seconds, text, callback) {
     let timer = seconds;
-    button.innerHTML = `<span>${text}</span> ${timer}`;
+    button.innerHTML = `<span>${text}</span> ${timer}`; //для отображения цикла и времени
     countdown = setInterval(() => {
 			timer--;
 			if (timer >= 0) {
-				button.innerHTML = `<span>${text}</span> ${timer}`;
+				button.innerHTML = `<span>${text}</span> ${timer}`; //для отображения цикла и времени
 			} else {
 				clearInterval(countdown);
 				callback();
